e0c1eff5a8819bfaae887ae098e5663fd22a6a8f
KhiemPham1702 <skhiem17022002@gmail.com>
2023-11-19 23:00:54 +0700

    update IDCardReader

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("net.sf.scuba:scuba-sc-android:0.0.20")
+    implementation("com.google.code.gson:gson:2.9.1")
+    implementation("com.google.guava:guava:32.0-jre")
+    implementation("com.google.guava:listenablefuture:1.100-jre")
+
+    testImplementation("org.mockito:mockito-core:4.8.1")
+
+    implementation("org.jmrtd:jmrtd:0.8.2")
+    implementation("com.madgag.spongycastle:prov:1.66.0.0")
+    implementation("com.github.mhshams:jnbis:1.1.0")
+
+    implementation(files("libs/vnidreader-library-1.0.1.aar"))
 }
--- app/src/main/java/com/example/mobile_seminar_project/IDCardReader.java
+++ app/src/main/java/com/example/mobile_seminar_project/IDCardReader.java
@@ -1,14 +1,15 @@
 package com.example.mobile_seminar_project;
 
-import android.graphics.Bitmap;
 import android.nfc.tech.IsoDep;
-import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 
 import com.example.mobile_seminar_project.model.CardResult;
-import com.example.mobile_seminar_project.model.IDCardDetail;
 import com.example.mobile_seminar_project.model.ResultCode;
+import com.htc.sdk.eidparser.ICaoReaderParser;
+import com.htc.sdk.model.IDCardDetail;
+
+import net.sf.scuba.smartcards.CardService;
 
 public class IDCardReader {
     private static final String TAG = IDCardReader.class.getSimpleName();
@@ -17,14 +18,14 @@ public class IDCardReader {
     }
 
 
-    public CardResult readData(CardService card, String cccdId, boolean hashCheck, boolean chipCheck, boolean activeCheck) {
-        return (new ICaoReaderParser()).readData(card, cccdId, hashCheck, chipCheck, activeCheck);
+    public com.htc.sdk.model.CardResult readData(CardService card, String cccdId, boolean hashCheck, boolean chipCheck, boolean activeCheck) {
+        return (new ICaoReaderParser()).readData(com.htc.sdk.scuba.smartcards.CardService.getInstance(card), cccdId, hashCheck, chipCheck, activeCheck);
     }
 
-    public CardResult readData(IsoDep isoDep, String cccdId, boolean hashCheck, boolean chipCheck, boolean activeCheck) {
+    public Object readData(IsoDep isoDep, String cccdId, boolean hashCheck, boolean chipCheck, boolean activeCheck) {
         try {
             CardService cardService = CardService.getInstance(isoDep);
-            return (new ICaoReaderParser()).readData(cardService, cccdId, hashCheck, chipCheck, activeCheck);
+            return (new ICaoReaderParser()).readData(com.htc.sdk.scuba.smartcards.CardService.getInstance(cardService), cccdId, hashCheck, chipCheck, activeCheck);
         } catch (Exception var7) {
             var7.printStackTrace();
             Log.e(TAG, "Error", var7);
