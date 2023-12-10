aa2f9074134bc5f947dad903b78b2e9ab0557704
KhiemPham1702 <skhiem17022002@gmail.com>
2023-12-10 10:42:04 +0700

    fix method channel

--- lib/nfc_plugin.dart
+++ lib/nfc_plugin.dart
@@ -1,8 +1,42 @@
+import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
 
 import 'nfc_plugin_platform_interface.dart';
 
 class NfcPlugin {
   Future<String?> getPlatformVersion() {
     return NfcPluginPlatform.instance.getPlatformVersion();
+  }
+
+  static const EventChannel nfcProgressChannel =
+      EventChannel('com.example.nfc_reader/progress');
+
+  Future<Map<dynamic, dynamic>?> sendDataToNative(String textFieldValue1,
+      String textFieldValue2, String textFieldValue3) async {
+    const MethodChannel platformChannel =
+        MethodChannel('com.example.nfc_reader/channel');
+    try {
+      final Map<String, String> data = {
+        'data1': textFieldValue1,
+        'data2': textFieldValue2,
+        'data3': textFieldValue3,
+      };
+      return await platformChannel.invokeMethod('sendDataToNative', data);
+    } on PlatformException catch (e) {
+      var logger = Logger();
+      logger.e("Error: ${e.message}");
+      return null;
+    }
+  }
+
+  static Future<String?> getNFCAvailability() async {
+    const MethodChannel platformChannel =
+        MethodChannel('com.example.nfc_reader/channel');
+    try {
+      final String? result =
+          await platformChannel.invokeMethod('getNFCAvailability');
+
+      return result;
+    } catch (ex) {}
   }
 }
